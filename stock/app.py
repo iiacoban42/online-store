a4358eebcde707d0401fe89938b492d2bd966128
Tiago <tiago.gomes@findmore.pt>
2022-05-24 19:15:26 +0200

    Stock Implementation

--- stock/app.py
+++ stock/app.py
@@ -1,40 +1,48 @@
-import os
-import atexit
-
+import threading
+from database import *
 from flask import Flask
 import redis
 
 
 app = Flask("stock-service")
 
-db: redis.Redis = redis.Redis(host=os.environ['REDIS_HOST'],
-                              port=int(os.environ['REDIS_PORT']),
-                              password=os.environ['REDIS_PASSWORD'],
-                              db=int(os.environ['REDIS_DB']))
+db = attempt_connect()
 
 
-def close_db_connection():
-    db.close()
+#threading.Thread(target=lambda: communicator.start_listening()).start()
 
-
-atexit.register(close_db_connection)
 
 
 @app.post('/item/create/<price>')
 def create_item(price: int):
-    pass
+    new_item_id = db.create_user(price)
+    return {
+        "item_id": new_item_id
+    }
 
 
 @app.get('/find/<item_id>')
 def find_item(item_id: str):
-    pass
+    item = db.find_item(item_id)
+    return {
+        "item_id": item.item_id,
+        "price": item.price,
+        "sotck": item.stock
+    }
 
 
 @app.post('/add/<item_id>/<amount>')
 def add_stock(item_id: str, amount: int):
-    pass
+    db.add_stock(item_id, amount)
+    return "Success"
 
 
 @app.post('/subtract/<item_id>/<amount>')
 def remove_stock(item_id: str, amount: int):
-    pass
+    item = db.find_item(item_id)
+    if item.stock >= amount:
+        db.remove_stock(item_id, amount)
+        return {
+            "Success"
+        }
+    return "Item not enough stock", 400
---
+++ stock/scripts.py
@@ -0,0 +1,18 @@
+create_table_script = """
+CREATE TABLE IF NOT EXISTS public.\"Stock\"
+(
+    item_id SERIAL,
+    price double precision NOT NULL DEFAULT 0,
+    stock INT NOT NULL DEFAULT 0,
+    CONSTRAINT Items_pkey PRIMARY KEY (item_id),
+    CONSTRAINT Price_positive CHECK ( price >= 0 ),
+    CONSTRAINT Stock_positive CHECK ( stock >= 0 )
+);"""
+
+insert_item_script = "INSERT INTO public.\"Stock\" (item_id, price, stock) VALUES (DEFAULT, %s, DEFAULT) RETURNING item_id;"
+
+find_item_script = "SELECT * FROM public.\"Stock\" WHERE item_id = %s;"
+
+add_item_stock_script = "UPDATE public.\"Stock\" SET stock = stock + %s WHERE item_id = %s;"
+
+remove_item_stock_script = "UPDATE public.\"Stock\" SET stock = stock - %s WHERE item_id = %s;"
