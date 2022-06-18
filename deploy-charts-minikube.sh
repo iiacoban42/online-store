538c333ceea27928c6fb62643097c27ab00786be
Paula Iacoban <i.p.iacoban@gmail.com>
2022-06-18 20:57:58 +0200

    Add ingress to deploy charts

--- deploy-charts-minikube.sh
+++ deploy-charts-minikube.sh
@@ -1,7 +1,10 @@
 #!/usr/bin/env bash
 
 helm repo add bitnami https://charts.bitnami.com/bitnami
+helm repo add ingress-nginx https://kubernetes.github.io/ingress-nginx
 helm repo update
+
+helm install -f helm-config/nginx-helm-values.yaml nginx ingress-nginx/ingress-nginx
 
 helm install -f helm-config/postgres-helm-values.yaml order-db-0 bitnami/postgresql
 helm install -f helm-config/postgres-helm-values.yaml order-db-1 bitnami/postgresql
