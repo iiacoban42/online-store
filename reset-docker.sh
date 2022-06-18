91679b6ea4e42837dd4ba4210e687535630f0665
Alex Jeleniewski <alexjan100@hotmail.com>
2022-06-18 14:41:44 +0200

    adjust more configs

--- reset-docker.sh
+++ reset-docker.sh
@@ -5,6 +5,3 @@ docker image rm user:latest
 docker image rm order:latest
 docker image rm stock:latest
 docker-compose up -d
-echo '{"order_id": 1}' > order/order_id.json
-echo '{"item_id": 1}' > stock/item_id.json
-echo '{"user_id": 1}' > payment/user_id.json
