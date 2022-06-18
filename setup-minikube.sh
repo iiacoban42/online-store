e90dccf2858674c0723c19440d365dd684eba5f0
Paula Iacoban <i.p.iacoban@gmail.com>
2022-06-18 21:44:29 +0200

    Expose services

--- setup-minikube.sh
+++ setup-minikube.sh
@@ -4,3 +4,10 @@ minikube start
 minikube addons enable ingress
 ./deploy-charts-minikube.sh
 kubectl apply -f ./k8s
+
+kubectl create deployment stock --image=stock --port=7000
+kubectl expose deployment stock
+kubectl create deployment payment --image=payment --port=6000
+kubectl expose deployment payment
+kubectl create deployment order --image=order --port=5000
+kubectl expose deployment order
