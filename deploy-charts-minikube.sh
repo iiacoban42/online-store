86e54cfee1d6ef23e9b5f5ce8736145362898341
Paula Iacoban <i.p.iacoban@gmail.com>
2022-06-18 17:02:17 +0200

    Add a db pod for each shard

--- deploy-charts-minikube.sh
+++ deploy-charts-minikube.sh
@@ -3,4 +3,8 @@
 helm repo add bitnami https://charts.bitnami.com/bitnami
 helm repo update
 
-helm install -f helm-config/postgres-helm-values.yaml postgres bitnami/postgresql
+helm install -f helm-config/postgres-helm-values.yaml order-db-0 bitnami/postgresql
+helm install -f helm-config/postgres-helm-values.yaml order-db-1 bitnami/postgresql
+helm install -f helm-config/postgres-helm-values.yaml order-db-2 bitnami/postgresql
+helm install -f helm-config/postgres-helm-values.yaml stock-db-0 bitnami/postgresql
+helm install -f helm-config/postgres-helm-values.yaml payment-db-0 bitnami/postgresql
--- setup-minikube.sh
+++ setup-minikube.sh
@@ -1,8 +1,25 @@
 #!/bin/bash
-minikube start
+# minikube start
 minikube addons enable ingress
 ./deploy-charts-minikube.sh
 cd k8s
+
+
+kubectl run postgresql-client --rm --tty -i --restart='Never' --namespace default --image bitnami/postgresql \
+    --env="POSTGRES_PASSWORD=test123" --command -- psql --host order-db-0 -U postgres -d 0 -p 5101 -c "create database 0"
+
+kubectl run postgresql-client --rm --tty -i --restart='Never' --namespace default --image bitnami/postgresql \
+    --env="POSTGRES_PASSWORD=test123" --command -- psql --host order-db-1 -U postgres -d 0 -p 5102 -c "create database 0"
+
+kubectl run postgresql-client --rm --tty -i --restart='Never' --namespace default --image bitnami/postgresql \
+    --env="POSTGRES_PASSWORD=test123" --command -- psql --host order-db-2 -U postgres -d 0 -p 5103 -c "create database 0"
+
+kubectl run postgresql-client --rm --tty -i --restart='Never' --namespace default --image bitnami/postgresql \
+    --env="POSTGRES_PASSWORD=test123" --command -- psql --host stock-db-0 -U postgres -d 0 -p 7101 -c "create database 0"
+
+kubectl run postgresql-client --rm --tty -i --restart='Never' --namespace default --image bitnami/postgresql \
+    --env="POSTGRES_PASSWORD=test123" --command -- psql --host payment-db-0 -U postgres -d 0 -p 6101 -c "create database 0"
+
 kubectl apply -f .
 cd ..
 eval $(minikube -p minikube docker-env)
