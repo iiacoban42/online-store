55749df8adede5a5bb5dfafa2d8082d598043dde
Alex Jeleniewski <alexjan100@hotmail.com>
2022-06-19 14:32:07 +0200

    add fix

--- setup-minikube.sh
+++ setup-minikube.sh
@@ -3,5 +3,6 @@
 minikube start
 minikube addons enable ingress
 ./deploy-charts-minikube.sh
+kubectl delete -A ValidatingWebhookConfiguration ingress-nginx-admission
 kubectl apply -f ./k8s
 kubectl port-forward --namespace=ingress-nginx service/ingress-nginx-controller 8080:80
